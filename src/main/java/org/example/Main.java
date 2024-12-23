152393726f8500f0a5c360938309c503be542292
kozae00 <rhwogus777@gmail.com>
2024-12-23 15:33:55 +0900

    commit4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,13 +16,20 @@ class App{
 
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명언 : ");
-        scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기. 엔터를 입력해야 입력이 완료됨.
+        while(true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
 
-        System.out.print("작가 : ");
-        scanner.nextLine();
+            if(command.equals("등록")) { // String일 때, '==' 가 아닌 .equals() 사용
+                System.out.print("명언 : ");
+                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 대기. 엔터를 입력해야 입력이 완료됨.
 
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+            // System.out.println("명령) 종료");
+        }
     }
 }
