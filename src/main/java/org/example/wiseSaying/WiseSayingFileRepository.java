f8005ca7e13972ca45210daba0d46e5db75ffbc8
kozae00 <rhwogus777@gmail.com>
2025-01-02 13:56:00 +0900

    commit42 : Override

---
+++ src/main/java/org/example/wiseSaying/WiseSayingFileRepository.java
@@ -0,0 +1,31 @@
+package org.example.wiseSaying;
+
+import java.util.ArrayList;
+
+public class WiseSayingFileRepository extends WiseSayingRepository {
+
+    @Override
+    public WiseSaying findById(int id) {
+        return null;
+    }
+
+    @Override
+    public WiseSaying update(WiseSaying wiseSaying) {
+        return null;
+    }
+
+    @Override
+    public WiseSaying add(String content, String author) {
+        return null;
+    }
+
+    @Override
+    public void remove(WiseSaying wiseSaying) {
+
+    }
+
+    @Override
+    public ArrayList<WiseSaying> findAll() {
+        return null;
+    }
+}
--- src/main/java/org/example/wiseSaying/WiseSayingService.java
+++ src/main/java/org/example/wiseSaying/WiseSayingService.java
@@ -4,38 +4,33 @@ import java.util.ArrayList;
 
 public class WiseSayingService {
 
-    private final WiseSayingRepository wiseSayingRepository;
+    private final WiseSayingRepository wiseSayingMemRepository;
 
-    public WiseSayingService(WiseSayingRepository wiseSayingRepository) {
-        this.wiseSayingRepository = wiseSayingRepository;
+    public WiseSayingService(WiseSayingRepository wiseSayingMemRepository) {
+        this.wiseSayingMemRepository = wiseSayingMemRepository;
     }
 
     public WiseSaying findById(int targetId) {
-        return wiseSayingRepository.findById(targetId);
+        return wiseSayingMemRepository.findById(targetId);
     }
 
     public void update(WiseSaying wiseSaying, String newContent, String newAuthor) {
 
-        // 비즈니스 로직(객체를 조립하거나 조작하는 것 - 비즈니스 로직)
         wiseSaying.setContent(newContent);
         wiseSaying.setAuthor(newAuthor);
 
-        // 객체를 저장 -> 데이터 저장 로직
-
-        // 비즈니스 로직 - 서비스
-        // 데이터 입출력 - 레포지터리
-        wiseSayingRepository.update(wiseSaying);
+        wiseSayingMemRepository.update(wiseSaying);
     }
 
     public WiseSaying add(String content, String author) {
-        return wiseSayingRepository.add(content, author);
+        return wiseSayingMemRepository.add(content, author);
     }
 
     public ArrayList<WiseSaying> findAll() {
-        return wiseSayingRepository.findAll();
+        return wiseSayingMemRepository.findAll();
     }
 
     public void remove(WiseSaying wiseSaying) {
-        wiseSayingRepository.remove(wiseSaying);
+        wiseSayingMemRepository.remove(wiseSaying);
     }
 }
